1f0a622a6f782843a2c9981721e70be90ce6743c
razevedo1994 <razevedo.contato@gmail.com>
2023-09-26 22:02:11 -0300

    tests: move test

--- 01_domain_model/test_batches.py
+++ 01_domain_model/test_batches.py
@@ -2,6 +2,15 @@ from datetime import date
 from model import Batch, OrderLine
 
 
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch = Batch("batch-001", "SMAILL-TABLE", qty=20, eta=date.today())
+    line = OrderLine("order-ref", "SMALL-TABLE", 2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
+
+
 def make_batch_and_line(sku, batch_qty, line_qty):
     return (
         Batch("batch-001", sku, batch_qty, eta=date.today()),
